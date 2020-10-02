a88355a6dd4186961b53a54ecdd9ac7f2f5747ef
HoduGwaja <honeybutter9250@gmail.com>
2020-10-02 15:50:30 +0900

    [commit | 2020.10.02 | added scrollbox]

--- html-dom/src/App.js
+++ html-dom/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 function App() {
   return (
-    <ValidationSample/>
+    <div>
+      <ScrollBox/>
+    </div>
   );
 }
 
